509854e1fb088bd0aa50b8a97cbd74f016b2facf
rajkumar-sasikumar <rajkumar.sasikumar@freshworks.com>
2020-04-12 18:30:28 +0530

    articles - edit and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,34 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      #redirect_to article_path(@article)
+      flash[:notice] = "Article was created successfully!"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article edited successfully!"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 end
